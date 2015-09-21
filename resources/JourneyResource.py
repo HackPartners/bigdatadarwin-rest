1c2bac15e1513cdee5e31a3c0d9b69920d3cc82d
Vlad Velici <vlad.velici@gmail.com>
2015-09-21 23:26:03 +0100

    Remove print()s

--- resources/JourneyResource.py
+++ resources/JourneyResource.py
@@ -31,7 +31,6 @@ class JourneyResource(Resource):
 
         service = args.service
         station = validate_tiploc(args.station)
-        print("params:", service, station)
 
         try:
             cancelled = self._get_cancellations(True, station, service)
@@ -52,7 +51,7 @@ class JourneyResource(Resource):
     def _get_cancellations(self, cancelled=False, tiploc=None, service=None):
 
         if not tiploc and not service:
-            raise Exception("Either service and/or service must be provided.")
+            raise Exception("Either service and/or tiploc must be provided.")
 
         query_params = CallingPoint.cancelled==cancelled
 
@@ -61,7 +60,6 @@ class JourneyResource(Resource):
 
         # In this query, we query for the "unique" number
         # of schedules that contain a cancellation.
-        print(tiploc)
         schedules_found = Schedule.select(
                         Schedule,
                         CallingPoint
@@ -73,7 +71,5 @@ class JourneyResource(Resource):
                         [Schedule.uid,
                         Schedule.rid]
                     )
-        print(schedules_found)
-        print(schedules_found.count())
 
         return schedules_found.count()
