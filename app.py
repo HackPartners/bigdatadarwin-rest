8d10dbd965b28a4d59edd6cfe0549a2997e0babe
Vlad Velici <vlad.velici@gmail.com>
2015-09-22 10:52:45 +0100

    Add Cancellations which can be filtered by day, tiploc, uid and/or rid.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import Flask
 from flask_restful import Api
 from resources.JourneyResource import JourneyResource
 from resources.OnTimeResource import OnTimeResource
+from resources.Cancellations import Cancellations
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,6 +24,8 @@ api.add_resource(JourneyResource, '/hist/1.0/journey')
 
 api.add_resource(OnTimeResource, '/hist/1.0/ontime')
 
+api.add_resource(Daily, '/hist/1.0/daily')
+
+api.add_resource(Cancellations, "/hist/1.0/cancellations")
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3001, debug=True)
-
---
+++ resources/Cancellations.py
@@ -0,0 +1,101 @@
+from flask_restful import Resource, reqparse, marshal_with, fields
+
+from common.util import api_bool, validate_tiploc
+
+from bigdatadarwin.models import Schedule, CallingPoint
+
+import datetime
+
+query_parser = reqparse.RequestParser()
+
+query_parser.add_argument(
+    'apiKey', dest='api_key',
+    required=True,
+    type=str, help='Your BigData Darwin API Key',
+)
+
+query_parser.add_argument(
+    'day', dest='day',
+    type=str, help='The day, as yyyy-mm-dd',
+)
+
+query_parser.add_argument(
+    'uid', dest='uid',
+    type=str, help='The TSDB ID (uid) of the service',
+)
+
+query_parser.add_argument(
+    'rid', dest='rid',
+    type=str, help='The RID of the service.',
+)
+
+query_parser.add_argument(
+    'tiploc', dest='tiploc',
+    type=str, help='Either the TIPLOC or CRS code for the station.',
+)
+
+
+class Cancellations(Resource):
+
+    def get(self):
+
+        args = query_parser.parse_args()
+
+        if args.day:
+            try:
+                args.day = datetime.datetime.strptime(args.day, "%Y-%m-%d")
+                args.day = args.day.date()
+            except ValueError:
+                return {
+                    "Error": "Day must be of format yyyy-mm-dd.",
+                }
+
+        if args.tiploc:
+            args.tiploc = validate_tiploc(args.tiploc)
+
+        try:
+            count_cancelled = self._get_cancellations(True, args)
+            count_fulfilled = self._get_cancellations(False, args)
+
+            return {
+                "cancelled": count_cancelled,
+                "fulfilled": count_fulfilled,
+            }
+
+        except Exception as e:
+            return {
+                "err": "Something went wrong, please try again.",
+            }
+
+    def _get_cancellations(self, cancelled, args):
+
+        query_params = CallingPoint.cancelled==cancelled
+
+        if args.day:
+            query_params = query_params & (Schedule.start_date==args.day)
+
+        if args.tiploc:
+            query_params = query_params & (CallingPoint.tiploc==args.tiploc)
+
+        if args.uid:
+            query_params = query_params & (CallingPoint.uid==args.uid)
+
+        if args.rid:
+            query_params = query_params & (CallingPoint.rid==args.rid)
+
+        res =  CallingPoint.select(
+            CallingPoint.id
+        ).join(
+            Schedule
+        ).where(
+            query_params
+        ).distinct(
+            [Schedule.uid,
+            Schedule.rid]
+        ).order_by(
+            [Schedule.uid.desc(),
+            Schedule.rid.desc(),
+            Schedule.id.desc()]
+        )
+
+        return res.count()
